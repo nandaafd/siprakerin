e18969bde23ccecce86100a6e75cfd4c2f1081e4
nandaafd <nandaafd.info@gmail.com>
2023-05-30 18:35:57 +0700

    mitra page clear

--- resources/views/siprakerin-page/mitra/index.blade.php
+++ resources/views/siprakerin-page/mitra/index.blade.php
@@ -4,12 +4,12 @@
     <div class="col" id="prakerin-header">
         <div class="row">
           <div class="col-9">
-            <h2 style="font-weight: 600;">Mulai <span style="color:#FF6B00;"> Mimpi Dan Petualanganmu </span> 
-              Dengan Mendaftar Prakerin Sekarang Juga!</h2>
-            <h4>Jangan sampai terlambat ya..</h4>
+            <h2 style="font-weight: 600;">SMKS Ihyaul Ulum Dukun Gresik Mencetak SDM Yang <span style="color:#FF6B00;">Unggul </span>
+              Dan <span style="color:#FF6B00;">Berkualitas</span>, Salah Satunya Dengan Cara Bekerja Sama Dengan Mitra Dunia Usaha Dunia Industri!</h2>
+            <h4>Wah, Hebat Sekali Ya SMK Kita..</h4>
           </div>
           <div class="col-3">
-            <img id="vector1" class="img-fluid" src="{{asset('images/vector1.png')}}" alt="" srcset="">
+            <img id="vector1" class="img-fluid" src="{{asset('images/vector5.jpg')}}" alt="" srcset="">
           </div>
         </div>
     </div>
@@ -17,7 +17,24 @@
 
 <div class="row">
     <div class="col" id="prakerin-page">
-        
+      <h3>Mitra-Mitra SMKS Ihyaul Ulum Dukun Gresik</h3>
+      <div class="container text-center">
+        <div class="row">
+          @foreach ($mitra as $data)
+          <div class="col-3 my-3" id="mitra-card">
+            <div id="content-mitra" class="row text-center mx-auto">
+                <img src="{{asset('storage/'. $data->foto_mitra)}}" alt="" id="foto-mitra" class="mx-auto img-fluid">
+                <h5 class="my-3" id="nama-mitra">{{$data->nama_mitra}}</h5>
+                <ul class="">
+                  <li>Alamat : {{$data->alamat}}</li>
+                  <li>Bidang : {{$data->bidang}}</li>
+                  <li>Kuota Siswa : {{$data->kuota}}</li>
+                </ul>
+              </div>
+          </div>
+          @endforeach
+        </div>
+      </div>
     </div>
 </div>
 @endsection
